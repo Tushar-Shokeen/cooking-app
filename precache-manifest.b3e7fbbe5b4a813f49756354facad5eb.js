a0209237aaf8c0b248768e12129235a515fd21dd
Tushar-Shokeen <tusharshokeen32@gmail.com>
2022-07-11 12:47:23 +0530

    Updates

---
+++ precache-manifest.b3e7fbbe5b4a813f49756354facad5eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa2745d25987f17f95229453a95ca30",
+    "url": "/cooking-app/index.html"
+  },
+  {
+    "revision": "303a8e9303697850a351",
+    "url": "/cooking-app/static/css/main.bf9a2ade.chunk.css"
+  },
+  {
+    "revision": "1799dc7126688759ba02",
+    "url": "/cooking-app/static/js/2.1b2cb5d4.chunk.js"
+  },
+  {
+    "revision": "303a8e9303697850a351",
+    "url": "/cooking-app/static/js/main.1db4bfad.chunk.js"
+  },
+  {
+    "revision": "390ad72c9ecac8ff51ca",
+    "url": "/cooking-app/static/js/runtime~main.205ca215.js"
+  },
+  {
+    "revision": "c256e2bbb027178153b5f727bf7ac841",
+    "url": "/cooking-app/static/media/GothamRounded-Bold.c256e2bb.woff"
+  },
+  {
+    "revision": "cf6af960be2a0419fe1c882a8e7e513f",
+    "url": "/cooking-app/static/media/GothamRounded-Bold.cf6af960.woff2"
+  },
+  {
+    "revision": "8e267ddb4734795dcfd59e6749b211b3",
+    "url": "/cooking-app/static/media/GothamRounded-Book.8e267ddb.woff2"
+  },
+  {
+    "revision": "e4d3e8a24917b96ed1521e519712af0e",
+    "url": "/cooking-app/static/media/GothamRounded-Book.e4d3e8a2.woff"
+  },
+  {
+    "revision": "d4f35094a237b47c54e335309b73efd3",
+    "url": "/cooking-app/static/media/GothamRounded-Light.d4f35094.woff2"
+  },
+  {
+    "revision": "f2d6aa9aeea75a50710093c59434472a",
+    "url": "/cooking-app/static/media/GothamRounded-Light.f2d6aa9a.woff"
+  }
+]);
